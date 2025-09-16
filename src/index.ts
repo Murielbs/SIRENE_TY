3d013a4725f2c74e8ae031c70b5f82f8867eb48e
Muriel <bezerramuriel8@gmail.com>
2025-09-16 16:25:40 +0000

    testes_0.1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("oii");
+import express,{Request, Response} from 'express'
+
+const app = express()
+
+let produts = []
+
+app.get('/produtos',(req: Request, res: Response) => {
+
+    const newProduct = {
+       name:"bateria",
+       price: 300,
+       description:{
+          street:"rua escritor",
+          number: 0,
+          state: "PE"
+        }
+
+    }
+    produts.push(newProduct)
+    return res.json(produts)
+})
+app.listen(3000,() => {
+    console.log('servidor rodando')
+})
